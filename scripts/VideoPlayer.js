6aba96e8b75a018dae2beafa9a5c2c55a8bf4428
Alexey Volchenkov <altay197@yandex.ru>
2025-05-20 01:03:34 +0300

    video-interview is finished

--- scripts/VideoPlayer.js
+++ scripts/VideoPlayer.js
@@ -1,12 +1,45 @@
-rootSelector = '[data-js-video-player]'
+const rootSelector = '[data-js-video-player]'
 
 class VideoPlayer {
   selector = {
     root: rootSelector,
+    video: '[data-js-video-player-video]',
+    panel: '[data-js-video-player-panel]',
+    playButton: '[data-js-video-player-play-button]',
+  }
+  
+  stateClasses = {
+    isActive: 'is-active',
   }
 
   constructor (rootElement) {
+    this.rootElement = rootElement
+    this.videoElement = this.rootElement.querySelector(this.selector.video)
+    this.panelElement = this.rootElement.querySelector(this.selector.panel)
+    this.playButtonElement = this.panelElement.querySelector(this.selector.playButton)
+    this.bindEvents()
+  }
 
+  onPlayButtonClick = (event) => {
+    event.preventDefault();
+    if (this.videoElement.paused) {
+      this.videoElement.play();
+      this.videoElement.controls = true
+      this.panelElement.classList.remove(this.stateClasses.isActive)
+    } else {
+      this.videoElement.pause();
+    }
+  }
+
+  onVideoPause = () => {
+    this.videoElement.controls = false
+    this.panelElement.classList.add(this.stateClasses.isActive)
+  }
+
+  bindEvents() {
+    this.playButtonElement.addEventListener('click', this.onPlayButtonClick)
+    this.videoElement.addEventListener('click', this.onPlayButtonClick)
+    this.videoElement.addEventListener('pause', this.onVideoPause)
   }
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import Header from './Header.js'
 import PaginationCollection from './Pagination.js'
+import VideoPlayerCollection from './VideoPlayer.js'
 
 new Header()
 new PaginationCollection()
+new VideoPlayerCollection()
