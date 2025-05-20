916391000dc59bbe6f57bbea6d56b7726f3efbee
Alexey Volchenkov <altay197@yandex.ru>
2025-05-20 21:42:14 +0300

    finished form email with validation and in generally done home page with some problems

---
+++ scripts/FormsValidation.js
@@ -0,0 +1,61 @@
+const rootSelector = '[data-js-form]'
+
+class FormsValidation {
+  selectors = {
+    root: rootSelector,
+    error: '[data-js-form-fields-error]',
+  }
+
+  states = {
+    invalid: 'invalid',
+  }
+
+  errorMessages = {
+    valueMissing: '123',
+  }
+
+  constructor (rootElement) {
+    this.rootElement = rootElement
+    this.errorElement = this.rootElement.querySelector(this.selectors.error)
+    this.bindEvents()
+  }
+
+  onBlur(event) {
+    const { target } = event
+    const isFormField = target.closest(this.selectors.root)
+    const isRequired = target.required
+
+    if (isFormField && isRequired) {
+      const isValid = target.value.includes('@');
+      target.classList.toggle(this.states.invalid, !isValid);
+    }
+  }
+
+  onFocus(event) {
+    event.target.classList.remove(this.states.invalid)
+  }
+
+  bindEvents() {
+    document.addEventListener('blur', (event) => {
+      this.onBlur(event)
+    }, { capture: true })
+
+    document.addEventListener('focus', (event) => {
+      this.onFocus(event)
+    }, { capture: true })
+  }
+}
+
+class FormsValidationCollection {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    document.querySelectorAll(rootSelector).forEach((element) => {
+      new FormsValidation(element)
+    })
+  }
+}
+
+export default FormsValidationCollection
--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ import Header from './Header.js'
 import PaginationCollection from './Pagination.js'
 import VideoPlayerCollection from './VideoPlayer.js'
 import AccordionCollection from './Accordion.js'
+import FormsValidationCollection from './FormsValidation.js'
 
 new Header()
 new PaginationCollection()
 new VideoPlayerCollection()
 new AccordionCollection()
+new FormsValidationCollection()
